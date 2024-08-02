d0afef603f9bb676ac60e9d6965cdc80d0711138
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-08-02 14:52:49 +0200

    Create listener
    Created a eventListener for all the buttons and
    made the logic when user clicks "=" to do the
    calculation

--- script.js
+++ script.js
@@ -14,7 +14,34 @@ function divide(num1, num2){
     return num1 / num2
 }
 
-console.log(add(6, 10))
-console.log(subtract(6, 10))
-console.log(multiply(6, 10))
-console.log(divide(6, 10))
+function operate(operator, num1, num2){
+    if(operator === "+"){
+        return add(num1, num2)
+    }else if(operator === "-"){
+        return subtract(num1, num2)
+    }else if(operator === "*"){
+        return multiply(num1, num2)
+    }else if(operator === "/"){
+        return divide(num1, num2)
+    }
+}
+
+const selectDisplay = document.querySelector(".display")
+const addPara = document.createElement("p")
+selectDisplay.appendChild(addPara)
+
+addPara.textContent = "123"
+let num1 = 5
+let num2 = 5
+let operator = "*"
+
+const selectButtons = document.querySelectorAll("button")
+
+selectButtons.forEach( (button) =>{
+    button.addEventListener("click", ()=>{
+        addPara.textContent += button.textContent.trim()
+        if(addPara.textContent.slice(-1) === "="){
+            addPara.textContent = operate(operator, num1, num2)
+        }
+    })
+})
