dd632ec577fb943ac4107947a7e8c4b46e62dfd9
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-08-02 14:29:37 +0200

    Create buttons and basic functions
    Created the buttons for the inputs and shaped the
    html with css to make it look like a calculator.
    Also made the basic functions for add,subtract,
    multiply and divide

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(num1, num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2
+}
+
+console.log(add(6, 10))
+console.log(subtract(6, 10))
+console.log(multiply(6, 10))
+console.log(divide(6, 10))
